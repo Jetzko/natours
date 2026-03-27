19b20f7b06eed0a945369ee248f5061a606533ce
[Andrea De Filippi] <[andreadefilippi68@gmail.com]>
2026-03-27 12:25:07 +0100

    Stripe impl. 3

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dns = require('dns');
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-process.on('uncaughtException', (err) => {
+process.on('uncaughtException', err => {
   console.log('UNCAUGHT EXEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
@@ -21,10 +21,10 @@ mongoose.connect(DB);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-  // console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', (err) => {
+process.on('unhandledRejection', err => {
   console.log('UNHANDLER REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
