a5f2f38ccd5bc2c38c91569bdfe107b114a4b7b8
[Andrea De Filippi] <[andreadefilippi68@gmail.com]>
2026-03-27 12:41:35 +0100

    Stripe messages

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -4,6 +4,13 @@ const Booking = require('../models/bookingModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
+exports.alert = (req, res, next) => {
+  const { alert } = req.query;
+  if (alert === 'booking')
+    res.locals.alert =
+      "Your booking was successfull! Please check your email for confirmation. If your booking doesn't show up here immediatly, please come back later.";
+};
+
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
   // console.log('ROUTE / HIT');
