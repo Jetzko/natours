a76404afdb8a06b6c1317f95d204ca7becbd9e0b
[Andrea De Filippi] <[andreadefilippi68@gmail.com]>
2026-03-27 11:02:09 +0100

    Added Sigterm closure

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dns = require('dns');
 
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-process.on('uncaughtException', err => {
+process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXEPTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   process.exit(1);
@@ -24,10 +24,17 @@ const server = app.listen(port, () => {
   // console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
+process.on('unhandledRejection', (err) => {
   console.log('UNHANDLER REJECTION! 💥 Shutting down...');
   console.log(err.name, err.message);
   server.close(() => {
     process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log('🚀SIGTERM RECEIVED. Shutting down gracefully.');
+  server.close(() => {
+    console.log('💥 Process terminated!');
   });
 });
